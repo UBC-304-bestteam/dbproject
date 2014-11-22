3b649d472661f4575c1689e3ede8af2d83ef02a2
Jagdeep Gill <jgill.tftf@gmail.com>
2014-11-22 15:25:00 -0800

    added order processing for manager

--- manager.php
+++ manager.php
@@ -1,33 +1,16 @@
-<!--
-	TODO add error viewing area
-	form and table to process the delivery of an order (it just sets the delivery date)
-	-->
-
 <html>
 <head>
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 
 <title>AMS Manager Interface</title>
-<!--
-    A simple stylesheet is provided so you can modify colours, fonts, etc.
--->
-    <link href="ams.css" rel="stylesheet" type="text/css">
+<link href="ams.css" rel="stylesheet" type="text/css">
 
-<!--
-    Javascript to submit a title_id as a POST form, used with the "delete" links
--->
+<!-- Javascript to write errors to a designated area -->
 <script>
-function formSubmit(titleId) {
-    'use strict';
-    if (confirm('Are you sure you want to delete this title?')) {
-      // Set the value of a hidden HTML element in this form
-      var form = document.getElementById('delete');
-      form.title_id.value = titleId;
-      // Post this form
-      form.submit();
-    }
-}
+	function writeMessage(errorString){
+		document.getElementById('errorarea').innerHTML += errorString;
+	}
 </script>
 </head>
 
@@ -41,151 +24,224 @@ function formSubmit(titleId) {
 	<a href="manager.php">Manager</a>
 <table class="headertable">
 <tr><td><h1>Manager Interface</h1></td></tr>
-<tr><td><div class="errorarea"> </div></td></tr>
+<tr><td><div id='errorarea' class="errorarea"> </div></td></tr>
 </table>
 </div>
+
 <?php
-    /****************************************************
-     STEP 1: Connect to the bookbiz MySQL database
-     ****************************************************/
 
-	 // CHANGE this to connect to your own MySQL instance in the labs or on your own computer
-    $connection = new mysqli("localhost:3307", "root", "", "bookbiz");
+// USEFUL GENERAL PROCEDURES
+function getConnection() {
+	return @new mysqli("localhost:3307", "root", "", "ams");
 
-    // Check that the connection was successful, otherwise exit
+}
+
+// write a message to the designated message area
+function writeMessage($msg) {
+	$asString = '"'.$msg.'"';
+	echo '<script> writeMessage('.$asString.');</script>'; 
+}
+
+// ARGS MUST BE PROVIDED! just list the name field for all required fields from a form like checkRequiredFields('a','b','c')
+// if any required fields are not present it writes an error.
+function checkRequiredFields() { 
+	// vals is an array containing all arguments to checkRequiredFields(...)
+	$vals = func_get_args();
+	$missingVals = '';
+	// add field names that are missing to the string
+	foreach($vals as $requiredval){
+		if (empty($_POST[$requiredval])) {
+			$missingVals .= $requiredval.', ';
+		}
+	}
+
+	// if there are missing vals then print an error message.
+	if (! empty($missingVals)){
+		$errorString = 'You forgot to enter these values: '.$missingVals;
+		writeMessage($errorString);
+		exit();
+	}
+}
+	
+	
+	
+// DEAL WITH UI REQUESTS HERE (like what do when user clicks a button to submit info)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+//	User clicked "Get Incomplete Orders" button so setup what should happen when page is refreshed	
+    if (isset($_POST["submit_view_incompleteorders"]) && $_POST["submit_view_incompleteorders"] == "Get Incomplete Orders") {
+		// just call function that gets and writes the results.
+       showIncompleteOrders();
+      }
+	  
+//	User clicked "Update order" button so setup what should happen when page is refreshed	
+    if (isset($_POST["submit_updateorder"]) && $_POST["submit_updateorder"] == "Update order") {
+		// just call function that gets and writes the results.
+       updateIncompleteOrder();
+      }
+   
+//	User clicked "Get Inventory" button
+	if (isset($_POST["submit_viewinventory"]) && $_POST["submit_viewinventory"] == "Get Inventory") {
+		// 2. call function that gets and writes the results.
+       showInventory();
+      }
+}
+   
+// FUNCTIONS THAT DEAL WITH DB REQUESTS
+
+function showIncompleteOrders(){
+	// need to get data, so get a connection to the DB
+    $connection = getConnection();
+
     if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
+        writeMessage("Could not connect to database");
         exit();
     }
-    /****************************************************
-     STEP 2: Detect the user action
-
-     Next, we detect what the user did to arrive at this page
-     There are 3 possibilities 1) the first visit or a refresh,
-     2) by clicking the Delete link beside a book title, or
-     3) by clicking the bottom Submit button to add a book title
-     
-     NOTE We are using POST superglobal to safely pass parameters
-        (as opposed to URL parameters or GET)
-     ****************************************************/
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-      if (isset($_POST["submitDelete"]) && $_POST["submitDelete"] == "DELETE") {
-       /*
-          Delete the selected book title using the title_id
-        */
-       
-       // Create a delete query prepared statement with a ? for the title_id
-       $stmt = $connection->prepare("DELETE FROM titles WHERE title_id=?");
-       $deleteTitleID = $_POST['title_id'];
-       // Bind the title_id parameter, 's' indicates a string value
-       $stmt->bind_param("s", $deleteTitleID);
-       
-       // Execute the delete statement
-       $stmt->execute();
-          
-       if($stmt->error) {
-         printf("<b>Error: %s.</b>\n", $stmt->error);
-       } else {
-         echo "<b>Successfully deleted ".$deleteTitleID."</b>";
-       }
-            
-      } elseif (isset($_POST["submit"]) && $_POST["submit"] ==  "ADD") {       
-       /*
-        Add a book title using the post variables title_id, title and pub_id.
-        */
-        $title_id = $_POST["new_title_id"];
-        $title = $_POST["new_title"];
-        $pub_id = $_POST["new_pub_id"];
-		$price = $_POST["new_price"];
-		$subtitle = $_POST["new_subtitle"];
-          
-        $stmt = $connection->prepare("INSERT INTO titles (title_id, title, pub_id, price, subtitle) VALUES (?,?,?,?,?)");
-          
-        // Bind the title and pub_id parameters, 'sss' indicates 3 strings
-        $stmt->bind_param("sssis", $title_id, $title, $pub_id, $price, $subtitle);
-        
-        // Execute the insert statement
-        $stmt->execute();
-          
-        if($stmt->error) {       
-          printf("<b>Error: %s.</b>\n", $stmt->error);
-        } else {
-          echo "<b>Successfully added ".$title."</b>";
-        }
-      }
-   }
-?>
-
-<h2>Book Titles in alphabetical order</h2>
-<!-- Set up a table to view the book titles -->
-<table border=0 cellpadding=0 cellspacing=0>
-<!-- Create the table column headings -->
-
-<tr valign=center>
-<td class=rowheader>UPC</td>
-<td class=rowheader>Title</td>
-<td class=rowheader>Publisher ID</td>
-<td class=rowheader>Price</td>
-<td class=rowheader>Subtitle</td>
-</tr>
-
-<?php
-    /****************************************************
-     STEP 3: Select the most recent list of book titles
-     ****************************************************/
-
-   // Select all of the book rows columns title_id, title and pub_id
-    if (!$result = $connection->query("SELECT title_id, title, pub_id, price, subtitle FROM titles ORDER BY title")) {
-        die('There was an error running the query [' . $db->error . ']');
+	
+    // make the query needed, it gives an array of rows from resulting query table
+    if (!$result = $connection->query("SELECT receiptId, odate, expectedDate, deliveredDate FROM orders WHERE expectedDate is null or deliveredDate is null")) {
+        writeMessage("Error making the query...");
+		exit();
     }
-
-    // Avoid Cross-site scripting (XSS) by encoding PHP_SELF (this page) using htmlspecialchars.
-    echo "<form id=\"delete\" name=\"delete\" action=\"";
-    echo htmlspecialchars($_SERVER["PHP_SELF"]);
-    echo "\" method=\"POST\">";
-    // Hidden value is used if the delete link is clicked
-    echo "<input type=\"hidden\" name=\"title_id\" value=\"-1\"/>";
-   // We need a submit value to detect if delete was pressed 
-    echo "<input type=\"hidden\" name=\"submitDelete\" value=\"DELETE\"/>";
-
-
-    /****************************************************
-     STEP 4: Display the list of book titles
-     ****************************************************/
-    // Display each book title databaserow as a table row
-    while($row = $result->fetch_assoc()){
-        
-       echo "<td>".$row['title_id']."</td>";
-       echo "<td>".$row['title']."</td>";
-       echo "<td>".$row['pub_id']."</td>";
-	   echo "<td>$".$row['price']."</td>";
-	   echo "<td>".$row['subtitle']."</td><td>";
-       
-       //Display an option to delete this title using the Javascript function and the hidden title_id
-       echo "<a href=\"javascript:formSubmit('".$row['title_id']."');\">DELETE</a>";
-       echo "</td></tr>";
-        
-    }
-    echo "</form>";
+	// set up the table
+	echo "<table><tr>
+			<td class=rowheader>#</td>
+			<td class=rowheader>Receipt ID</td>
+			<td class=rowheader>Order Date</td>
+			<td class=rowheader>Expected Delivery Date</td>
+			<td class=rowheader>Delivered Date</td>
+			</tr>";
+	// now write each row from result as a row in the html table
+	if ($result->num_rows == 0){
+		// if there's no such orders just write an error
+		writeMessage("No incomplete orders");
+		exit();
+	} else {
+		while($row = $result->fetch_assoc()){
+			$i = 1;
+			echo "<tr>";
+			echo "<td>".$i."</td>";
+			echo "<td>".$row['receiptId']."</td>";
+			echo "<td>".$row['odate']."</td>";
+			echo "<td>".$row['expectedDate']."</td>";
+			echo "<td>".$row['deliveredDate']."</td>";
+			echo "</tr>";
+			$i += 1;
+		}
+	}
+	echo "</table>";
 
     // Close the connection to the database once we're done with it.
     mysqli_close($connection);
-?>
+   }
+   
+function showInventory(){
+	// need to get data, so get a connection to the DB
+    $connection = getConnection();
 
-</table>
+    if (mysqli_connect_errno()) {
+        writeMessage("Could not connect to database");
+        exit();
+    }
+	
+    // make the query needed, it gives an array of rows from resulting query table
+    if (!$result = $connection->query("SELECT * 
+										FROM item")) {
+        writeMessage("Error making the query...");
+		exit();
+    }
+	// set up the table
+	echo "<table><tr>
+			<td class=rowheader>#</td>
+			<td class=rowheader>UPC</td>
+			<td class=rowheader>Title</td>
+			<td class=rowheader>Item type</td>
+			<td class=rowheader>Category</td>
+			<td class=rowheader>Company</td>
+			<td class=rowheader>Release year</td>
+			<td class=rowheader>Price</td>
+			<td class=rowheader>Stock</td>
+			</tr>";
+	// now write each row from result as a row in the html table
+	if ($result->num_rows == 0){
+		// if there's no such orders just write an error
+		writeMessage("No inventory!");
+		exit();
+	} else {
+		while($row = $result->fetch_assoc()){
+			$i = 1;
+			echo "<tr>";
+			echo "<td>".$i."</td>";
+			echo "<td>".$row['upc']."</td>";
+			echo "<td>".$row['title']."</td>";
+			echo "<td>".$row['item_type']."</td>";
+			echo "<td>".$row['category']."</td>";
+			echo "<td>".$row['company']."</td>";
+			echo "<td>".$row['release_year']."</td>";
+			echo "<td>$".$row['price']."</td>";
+			echo "<td>".$row['stock']."</td>";
+			echo "</tr>";
+			$i += 1;
+		}
+	}
+	echo "</table>";
+
+    // Close the connection to the database once we're done with it.
+    mysqli_close($connection);
+   }
+
+function updateIncompleteOrder(){
+	// receiptid, new_expecteddate, new deliverdate
+	// need to get data, so get a connection to the DB
+    $connection = getConnection();
+
+    if (mysqli_connect_errno()) {
+        writeMessage("Could not connect to database");
+        exit();
+    }
+
+	// must at least enter a receiptid so check that value was entered
+	checkRequiredFields('receiptid','new_expecteddate'); // value of the name field from the form
+	
+	// get the values user entered in the form
+	$receiptid = $_POST['receiptid'];
+	$expecteddate = $_POST['new_expecteddate'];
+	$delivereddate = $_POST['new_deliverdate'];
+	
+	// these values might be strings, then the date is updated to empty string equivalent 0000-00-00
+	// so make them NULL instead.
+	if(empty($expecteddate)){
+		$expecteddate = NULL;
+	}
+	if(empty($delivereddate)){
+		$delivereddate = NULL;
+	}
+	
+	// make the query needed, it gives an array of rows from resulting query table
+	$stmt = $connection->prepare('UPDATE orders
+								  SET expectedDate = ?, deliveredDate = ?
+								  WHERE receiptId = ?');
+	$stmt->bind_param("ssi",$expecteddate,$delivereddate,$receiptid);
+    $stmt->execute();
+	
+	// make sure a row was actually changed, e.g. 0 rows are changed if receiptId entered does not exist!
+	if ($stmt->affected_rows == 0){
+		writeMessage("Receipt ID does not exist.");
+		exit();
+	}
+	// tell user what happened
+	if($stmt->error) {       
+		writeMessage("Error when updating the order:".$stmt->error);
+	} else {
+         writeMessage("Successfully updated the order");
+	}
+}
+?>
 
 
 <!--
-  /****************************************************
-   STEP 5: Build the form to add a book title
-   ****************************************************/
-    Use an HTML form POST to add a book, sending the parameter values back to this page.
-    Avoid Cross-site scripting (XSS) by encoding PHP_SELF using htmlspecialchars.
-
-    This is the simplest way to POST values to a web page. More complex ways involve using
-    HTML elements other than a submit button (eg. by clicking on the delete link as shown above).
+ USER CONTROLS ARE HERE
 -->
 <table>
 <tr>
@@ -194,8 +250,8 @@ function formSubmit(titleId) {
 <h2>Add a New Item:</h2>
 <form id="add" name="add" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <table border=0 cellpadding=0 cellspacing=0>
-        <tr><td>UPC:</td><td><input type="text" size=30 name="new_upc"</td></tr>
-        <tr><td>Title:</td><td><input type="text" size=30 name="new_title"</td></tr>
+        <tr><td>UPC:</td><td><input type="text" size=30 name="new_upc"></td></tr>
+        <tr><td>Title:</td><td><input type="text" size=30 name="new_title"></td></tr>
         <tr><td>Type:</td><td> <input type="text" size=30 name="new_type"></td></tr>
         <tr><td>Category:</td><td> <input type="text" size=30 name="new_category"></td></tr>
         <tr><td>Company:</td><td> <input type="text" size=30 name="new_company"></td></tr>
@@ -212,8 +268,8 @@ function formSubmit(titleId) {
 <h2>See Top Sellers:</h2>
 <form id="top" name="top" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <table border=0 cellpadding=0 cellspacing=0>
-        <tr><td>Date:</td><td><input type="text" size=30 name="date"</td></tr>
-        <tr><td>How many?:</td><td><input type="text" size=30 name="topnum"</td></tr>
+        <tr><td>Date:</td><td><input type="text" size=30 name="date" value="YYYY-MM-DD"></td></tr>
+        <tr><td>How many?:</td><td><input type="text" size=30 name="topnum"></td></tr>
         <tr><td></td><td><input type="submit" name="submit_topsellers" border=0 value="Get Top Sellers"></td></tr>
     </table>
 </form>
@@ -224,7 +280,7 @@ function formSubmit(titleId) {
 <h2>See Daily Sales Report:</h2>
 <form id="daily" name="daily" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <table border=0 cellpadding=0 cellspacing=0>
-        <tr><td>Date:</td><td><input type="text" size=30 name="date"</td></tr>
+        <tr><td>Date:</td><td><input type="text" size=30 name="date" value="YYYY-MM-DD"></td></tr>
         <tr><td></td><td><input type="submit" name="submit_dailysales" border=0 value="Get Sales Report"></td></tr>
     </table>
 </form>
@@ -235,7 +291,27 @@ function formSubmit(titleId) {
 <h2>View Inventory:</h2>
 <form id="view" name="view" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <table border=0 cellpadding=0 cellspacing=0>
-        <tr><td></td><td><input type="submit" name="submit_viewinventory" border=0 value="Get Inventory Table"></td></tr>
+        <tr><td></td><td><input type="submit" name="submit_viewinventory" border=0 value="Get Inventory"></td></tr>
+    </table>
+</form>
+</td>
+
+
+
+<!-- process orders -->
+<td>
+<h2>Process Orders:</h2>
+<form id="view_incompleteorders" name="view_incompleteorders" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <table border=0 cellpadding=0 cellspacing=0>
+        <tr><td></td><td><input type="submit" name="submit_view_incompleteorders" border=0 value="Get Incomplete Orders"></td></tr>
+    </table>
+</form>
+<form id="updateorder" name="updateorder" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <table border=0 cellpadding=0 cellspacing=0>
+        <tr><td>Receipt ID:</td><td><input type="text" size=30 name="receiptid"></td></tr>
+        <tr><td>Expected delivery date:</td><td><input type="text" size=30 name="new_expecteddate" value="YYYY-MM-DD"></td></tr>
+        <tr><td>Delivered date:</td><td><input type="text" size=30 name="new_deliverdate" value="YYYY-MM-DD"></td></tr>
+        <tr><td></td><td><input type="submit" name="submit_updateorder" border=0 value="Update order"></td></tr>
     </table>
 </form>
 </td>
